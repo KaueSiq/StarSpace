26764a3f030f902ce9aa45127234029b623a0b5e
KaueSiq <1129646@atitus.edu.br>
2023-05-28 18:10:30 -0300

    start

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import pygame
 
 pygame.init()
+tamanho = (800,700)
+branco = (255,255,255)
+tela = pygame.display.set_mode(tamanho)
+pygame.display.set_caption("Star Link")
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
