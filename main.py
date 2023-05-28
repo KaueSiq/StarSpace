df30e7d1b63a2c8f769f3e69e9a25e06f2c9bd71
ViniZERAAAA <viniciuscasturino1551@gmail.com>
2023-05-28 18:28:55 -0300

    Update main.py
    fazendo a tela que abre quando clica na tela
    
    Co-Authored-By: KaueSiq <132311267+KaueSiq@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
+from  tkinter import simpledialog
 
 pygame.init()
-tamanho = (800,700)
+tamanho = (1200,700)
 branco = (255,255,255)
 tela = pygame.display.set_mode(tamanho)
 pygame.display.set_caption("Star Link")
@@ -12,6 +13,15 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
+        elif event.type ==  pygame.MOUSEBUTTONUP:
+            pos = pygame .mouse.get_pos()
+            item = simpledialog.askstring("space ", "Nome da Estrela:")
+            print (item)
+            if item == None:
+                item = "desconhecido" + str (pos)
+            estrelas[item] = pos
+
+
 
 
 
